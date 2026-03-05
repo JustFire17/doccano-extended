f806b8c33832c1bd418bf70f995cd1c34331f440
JustFire17 <129631432+a79890@users.noreply.github.com>
2026-03-05 15:15:31 +0000

    doccano extension Project

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -1,10 +1,12 @@
+import os
+
 from .base import *  # noqa: F403
 
 MIDDLEWARE.append("api.middleware.RangesMiddleware")  # noqa: F405
 
 # Avoid noisy staticfiles warning in development when frontend build artifacts
 # are not present yet.
-STATICFILES_DIRS = [static_dir for static_dir in STATICFILES_DIRS if path.isdir(static_dir)]  # noqa: F405
+STATICFILES_DIRS = [static_dir for static_dir in STATICFILES_DIRS if os.path.isdir(static_dir)]  # noqa: F405
 # LOGGING = {
 #     'version': 1,
 #     'handlers': {
